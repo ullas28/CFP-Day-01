fbf1a7fab9513da0e6ec5fa7eb5078cccda3baf0
unknown <you@example.com>
2022-12-12 15:48:00 +0530

    Added : established server connection

---
+++ src/com/bridgelabz/SimpleHttpServer.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class SimpleHttpServer {
+    public static int DEFAULT_PORT = 9000;
+    public static int port;
+    private HttpServer httpServer;
+
+    private void start(int port){
+        this.port = port;
+        try{
+            httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+            System.out.println("server started at " + port);
+            httpServer.createContext("/", new Handlers.RootHandler());
+            httpServer.createContext("/echoHeader", new Handlers.EchoHeaderHandler());
+            httpServer.createContext("/echoGet", new Handlers.EchoGetHandler());
+            httpServer.createContext("/echoPost", new Handlers.EchoPostHandler());
+            httpServer.setExecutor(null);
+            httpServer.start();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        SimpleHttpServer httpsServer = new SimpleHttpServer();
+        httpsServer.start(DEFAULT_PORT);
+    }
+}
